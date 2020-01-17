74ff36c243b6b6d1774edf3b3caf5a1e1e21227a
Julio <JulioToboso@gmail.com>
2020-01-18 00:53:45 +0100

    Create pkrability.adb

---
+++ pkrability.adb
@@ -0,0 +1,12 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+
+package body PKRability is
+
+function Provability return probability_tp is
+begin -- Provability
+   return 0.5;
+end Provability;
+
+
+end PKRability;
