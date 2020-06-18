36c9d7f3682a87730d43ae04f4874c76fba52fcc
Julio <JulioToboso@gmail.com>
2020-06-18 15:20:58 +0200

    Create NewBaraja.m

---
+++ Matlab/NewBaraja.m
@@ -0,0 +1,11 @@
+function Baraja=NewBaraja()
+%Devuelve una matriz con unos para representar las cartas
+    %La columna representa el palo
+    %La fila representa el valor
+Valor=13;
+Palo=4;
+
+Baraja=ones(Valor,Palo);
+ 
+
+end
