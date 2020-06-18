9d3e05d4725653ab586012628609ef8583f62252
Julio <JulioToboso@gmail.com>
2020-06-18 15:20:18 +0200

    Create NewPlayer5k.m

---
+++ Matlab/NewPlayer5k.m
@@ -0,0 +1,14 @@
+function Player = NewPlayer5k( name, number )
+%Makes a NewPlayer
+
+Player.number=number;
+Player.name=char(name);
+Player.money=5000;
+Player.in=0;
+Player.C1=CartaNull();
+Player.C2=CartaNull();
+Player.hand='null';
+Player.UpTo='null';
+
+
+end
